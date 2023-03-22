b1d9bb13efb994c3f4d255203fbff428fd4db2e1
19851806282 <2582031006@qq.com>
2023-03-22 10:24:34 +0800

    'first'

--- src/components/userList/search.vue
+++ src/components/userList/search.vue
@@ -1,58 +1,70 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
-import { useSchoolStore } from '../../store/school'
-import { useClsStore } from '../../store/cls'
-import { useStudentStore } from '../../store/student'
-const SchoolStore = useSchoolStore()
-const ClsStore = useClsStore()
-const StudentStore = useStudentStore()
- 
-const isExpansion = ref(false)
-StudentStore.searchStuImp.schoolId = SchoolStore.schoolList[0].schoolId
-StudentStore.search()
+import { reactive, ref } from "vue";
+import { useSchoolStore } from "../../store/school";
+import { useClsStore } from "../../store/cls";
+import { useStudentStore } from "../../store/student";
+const SchoolStore = useSchoolStore();
+const ClsStore = useClsStore();
+const StudentStore = useStudentStore();
 
-
+const isExpansion = ref(false);
+StudentStore.searchStuImp.schoolId = SchoolStore.schoolList[0].schoolId;
+StudentStore.search();
 </script>
 
 <template>
-    <el-card class="box-card">
-        <el-form :inline="true" :model="StudentStore.searchStuImp" class="all-form">
-            <el-form-item label="学校" >
-                <el-select v-model="StudentStore.searchStuImp.schoolId" placeholder="请选择">
-                    <el-option :label="school.schoolName" :value="school.schoolId" v-for="school in SchoolStore.schoolList"/>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="姓名" v-show="isExpansion">
-                <el-input v-model="StudentStore.searchStuImp.name" placeholder="请输入" />
-            </el-form-item>
-            <el-form-item label="手机号" v-show="isExpansion">
-                <el-input v-model="StudentStore.searchStuImp.name" placeholder="请输入" />
-            </el-form-item>
-            <el-form-item label="邮箱" v-show="isExpansion">
-                <el-input v-model="StudentStore.searchStuImp.name" placeholder="请输入" />
-            </el-form-item>
-            <el-form-item class="right-bottom">
-                <el-button @click="StudentStore.clear()">重置</el-button>
-                <el-button type="primary" @click="StudentStore.search()">查询</el-button>
-                <el-button @click="isExpansion=!isExpansion">
-                    <span v-show="isExpansion">收起</span>
-                    <span v-show="!isExpansion">展开</span>
-                </el-button>
-            </el-form-item>
-        </el-form>
-
-    </el-card>
-
+  <el-card class="box-card">
+    <el-form :inline="true" :model="StudentStore.searchStuImp" class="all-form">
+      <el-form-item label="学校">
+        <el-select
+          v-model="StudentStore.searchStuImp.schoolId"
+          placeholder="请选择"
+        >
+          <el-option
+            :label="school.schoolName"
+            :value="school.schoolId"
+            v-for="school in SchoolStore.schoolList"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="姓名" v-show="isExpansion">
+        <el-input
+          v-model="StudentStore.searchStuImp.name"
+          placeholder="请输入"
+        />
+      </el-form-item>
+      <el-form-item label="手机号" v-show="isExpansion">
+        <el-input
+          v-model="StudentStore.searchStuImp.name"
+          placeholder="请输入"
+        />
+      </el-form-item>
+      <el-form-item label="邮箱" v-show="isExpansion">
+        <el-input
+          v-model="StudentStore.searchStuImp.name"
+          placeholder="请输入"
+        />
+      </el-form-item>
+      <el-form-item class="right-bottom">
+        <el-button @click="StudentStore.clear()">重置</el-button>
+        <el-button type="primary" @click="StudentStore.search()"
+          >查询</el-button
+        >
+        <el-button @click="isExpansion = !isExpansion">
+          <span v-show="isExpansion">收起</span>
+          <span v-show="!isExpansion">展开</span>
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <style scoped>
-.all-form{
-    position:relative;
+.all-form {
+  position: relative;
 }
-.right-bottom{
-    position:absolute;
-    right:0px;
+.right-bottom {
+  position: absolute;
+  right: 0px;
 }
-
 </style>
-
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,106 +1,112 @@
-import { defineStore } from 'pinia';
-import axios from 'axios'
+import { defineStore } from "pinia";
+import axios from "axios";
 
 // 用户接口
 interface UserInfo {
-    _id: string,
-    schoolIdList: [],
-    avatar: string,
-    isDeleted: boolean,
-    manageClassList: [],
-    teachClassList: [],
-    isSpecialTeach: true,
-    specialStuIdList: [],
-    childStuIdList: [],
-    authSystemList: [],
-    totalVisitCount: number,
-    exp: number,
-    role: string,
-    account: string,
-    name: string,
-    sex: string,
-    phone: string,
-    schoolId: string,
-    schoolName: string,
-    customList: [],
-    password: string,
-    createdAt: string,
-    updatedAt: string,
-    __v: number,
-    lastVisitDate: string,
-    lastVisitIp: string
+  _id: string;
+  schoolIdList: [];
+  avatar: string;
+  isDeleted: boolean;
+  manageClassList: [];
+  teachClassList: [];
+  isSpecialTeach: true;
+  specialStuIdList: [];
+  childStuIdList: [];
+  authSystemList: [];
+  totalVisitCount: number;
+  exp: number;
+  role: string;
+  account: string;
+  name: string;
+  sex: string;
+  phone: string;
+  schoolId: string;
+  schoolName: string;
+  customList: [];
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  lastVisitDate: string;
+  lastVisitIp: string;
 }
 
-
 interface SearchUserImp {
-    current?: number,
-    pageSize?: number,
-    schoolId?: string[] | string,
-    name?: string,
-    phone?: string,
-    email?: string,
-    pageNo?: string
+  current?: number;
+  pageSize?: number;
+  schoolId?: string[] | string;
+  name?: string;
+  phone?: string;
+  email?: string;
+  pageNo?: string;
 }
 
 interface UpdateUserPassword {
-    OriginalPassword: string, // 原来的密码
-    NewPassword: string, // 新密码
-    ConfirmPassword: string // 确认密码
+  OriginalPassword: string; // 原来的密码
+  NewPassword: string; // 新密码
+  ConfirmPassword: string; // 确认密码
 }
 
-
 // 存储当前用户信息
-export const useUserStore = defineStore('user', {
-    state: () => {
-        return {
-            userInfo: {} as UserInfo,
-            count: 1,
-            searchUserImp: {} as SearchUserImp,
-            userList: [] as UserInfo[],
-            Total: 0,
-            userUpdateFrom: {} as UserInfo,
-            password: {} as UpdateUserPassword
-        }
-    },
+export const useUserStore = defineStore("user", {
+  state: () => {
+    return {
+      userInfo: {} as UserInfo,
+      count: 1,
+      searchUserImp: {} as SearchUserImp,
+      userList: [] as UserInfo[],
+      Total: 0,
+      userUpdateFrom: {} as UserInfo,
+      password: {} as UpdateUserPassword,
+    };
+  },
 
-    getters: {
+  getters: {},
 
-    },
-
-    actions: {
-        // 获取UserInfo
-        async loadUserInfo() {
-            var config = {
-                method: 'post',
-                maxBodyLength: Infinity,
-                url: 'https://www.yirenyian.com/api/user/getUserInfo',
-                headers: {
-                    'accessKey': '5033b7eef85f81bbdf40df0fd5c383406611393d6b8775468d19e3a01fc91a17a9523e204e8e1e39d57a1bc58fb1c3fac5d8706708375549b8209e5879461b6022d57be79ad8fa5d5073447c7bf027c070e917996fe34bae69ed1a8d4afcf824c3e59a43bf623c8806a64c95e882706d'
-                }
-            };
-            const { data: { data } } = await axios(config);
-            this.userInfo = data
-            this.userUpdateFrom = {...data}
-
+  actions: {
+    // 获取UserInfo
+    async loadUserInfo() {
+      var config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "https://www.yirenyian.com/api/user/getUserInfo",
+        headers: {
+          accessKey:
+            "5033b7eef85f81bbdf40df0fd5c383406611393d6b8775468d19e3a01fc91a17a9523e204e8e1e39d57a1bc58fb1c3fac5d8706708375549b8209e5879461b6022d57be79ad8fa5d5073447c7bf027c070e917996fe34bae69ed1a8d4afcf824c3e59a43bf623c8806a64c95e882706d",
         },
-        async searchUserInfo() {
-            if(this.searchUserImp.schoolId == undefined || this.searchUserImp.schoolId.length == 0 ){
-                this.searchUserImp.schoolId = ["5f4c6b2857852c176c03aacf"]
-            }
-            var config = {
-                method: 'post',
-                maxBodyLength: Infinity,
-                url: 'https://www.yirenyian.com/api/user/getList',
-                headers: {
-                    'accessKey': '5033b7eef85f81bbdf40df0fd5c383406611393d6b8775468d19e3a01fc91a17a9523e204e8e1e39d57a1bc58fb1c3fac5d8706708375549b8209e5879461b6022d57be79ad8fa5d5073447c7bf027c070e917996fe34bae69ed1a8d4afcf824c3e59a43bf623c8806a64c95e882706d',
-                    'Content-Type': 'application/json'
-                },
-                data: this.searchUserImp,
-            };
-            const { data: { data } } = await axios(config);            
-            this.userList = data.rows
-            this.Total = data.total
-            console.log(this.userList)
-        }
-    }
-})
+      };
+      const {
+        data: { data },
+      } = await axios(config);
+      this.userInfo = data;
+      this.userUpdateFrom = { ...data };
+      //   console.log(this.userInfo);
+      localStorage.setItem("username", JSON.stringify(this.userInfo.name));
+    },
+    async searchUserInfo() {
+      if (
+        this.searchUserImp.schoolId == undefined ||
+        this.searchUserImp.schoolId.length == 0
+      ) {
+        this.searchUserImp.schoolId = ["5f4c6b2857852c176c03aacf"];
+      }
+      var config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "https://www.yirenyian.com/api/user/getList",
+        headers: {
+          accessKey:
+            "5033b7eef85f81bbdf40df0fd5c383406611393d6b8775468d19e3a01fc91a17a9523e204e8e1e39d57a1bc58fb1c3fac5d8706708375549b8209e5879461b6022d57be79ad8fa5d5073447c7bf027c070e917996fe34bae69ed1a8d4afcf824c3e59a43bf623c8806a64c95e882706d",
+          "Content-Type": "application/json",
+        },
+        data: this.searchUserImp,
+      };
+      const {
+        data: { data },
+      } = await axios(config);
+      this.userList = data.rows;
+      this.Total = data.total;
+      console.log(this.userList);
+    },
+  },
+});
